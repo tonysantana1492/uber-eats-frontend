c2570bd9911a22a7925a3f247be455193af4e619
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-04-12 17:15:08 -0400

    feat(): add apollo provider to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
+import './styles/tailwind.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { ApolloProvider } from '@apollo/client';
+
+import { client } from './apollo';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<React.StrictMode>
-		<App />
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
 	</React.StrictMode>,
 );
 
