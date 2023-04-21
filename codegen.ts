e2c444dde0f1ac5215c235e75f21be1e5e0d1053
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-04-21 18:41:03 -0400

    build(): create config file for @graphql-codegen library

---
+++ codegen.ts
@@ -0,0 +1,18 @@
+import { CodegenConfig } from '@graphql-codegen/cli'
+ 
+const config: CodegenConfig = {
+  schema: 'http://localhost:4000/graphql',
+  documents: ['src/**/*.{tsx,ts}'],
+  ignoreNoDocuments: true, // for better experience with the watcher
+  generates: {
+    './src/gql/': {
+      preset: 'client',
+      presetConfig: {
+        fragmentMasking: false
+      }
+    },
+  },
+
+}
+ 
+export default config
