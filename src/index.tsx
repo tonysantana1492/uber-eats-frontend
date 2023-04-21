ea27c8b68fb9ed7617252034ce364c408c5c95eb
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-04-21 19:06:24 -0400

    feat(): add BrowserRouter for use react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,24 @@
 import './styles/tailwind.css';
 
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { ApolloProvider } from '@apollo/client';
 
 import { client } from './apollo';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HelmetProvider } from 'react-helmet-async';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-	<React.StrictMode>
-		<ApolloProvider client={client}>
-			<App />
-		</ApolloProvider>
-	</React.StrictMode>,
+	<ApolloProvider client={client}>
+		<HelmetProvider>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</HelmetProvider>
+	</ApolloProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
