125766e174933ccf21ca4ab6721e3242c03ae9a0
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-05-01 19:02:39 -0400

    fix(pages): change title of the page

--- src/pages/user/edit-profile.tsx
+++ src/pages/user/edit-profile.tsx
@@ -82,7 +82,7 @@ export const EditProfile: FC = () => {
 	return (
 		<div className='mt-52 flex flex-col justify-center items-center'>
 			<Helmet>
-				<title>Edit Profile | Nuber Eats</title>
+				<title>Edit Profile | Uber Eats</title>
 			</Helmet>
 			<h4 className='font-semibold text-2xl mb-3'>Edit Profile</h4>
 			<form noValidate onSubmit={handleSubmit(onSubmit)} className='grid max-w-screen-sm gap-3 mt-5 w-full mb-5'>
