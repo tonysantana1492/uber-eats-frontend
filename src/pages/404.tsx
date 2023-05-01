686c46a84199df8a4a190c5e7a7cf0d425d44010
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-05-01 19:02:11 -0400

    fix(pages): change title of the page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export const NotFound: FC = () => (
 	<div className='h-full flex flex-col items-center justify-center'>
 		<Helmet>
-			<title>Not Found | Nuber Eats</title>
+			<title>Not Found | Uber Eats</title>
 		</Helmet>
 		<h2 className='font-semibold text-2xl mb-3'>Page Not Found.</h2>
 		<h4 className='font-medium text-base mb-5'>{"The page you're looking for does not exist or has moved."}</h4>
