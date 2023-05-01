92c1354d9a4e8a5c07952e533be95d007e1d81fa
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-05-01 18:53:35 -0400

    test(): create unit test for Header component

--- src/components/__tests__/header.spec.tsx
+++ src/components/__tests__/header.spec.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, waitForElementToBeRemoved } from '@testing-library/react';
 import { MockedProvider } from '@apollo/client/testing';
 
 import { Header } from '../header';
@@ -6,8 +6,39 @@ import { BrowserRouter } from 'react-router-dom';
 import { ME_QUERY } from '../../hooks/useMe';
 
 describe('<Header />', () => {
-	it('renders OK with props', () => {
-		const { getByText } = render(
+	it('renders verify banner', async () => {
+		const { queryByText } = render(
+			<MockedProvider
+				mocks={[
+					{
+						request: {
+							query: ME_QUERY,
+						},
+						result: {
+							data: {
+								me: {
+									id: 1,
+									email: '',
+									role: '',
+									verified: false,
+								},
+							},
+						},
+					},
+				]}
+			>
+				<BrowserRouter>
+					<Header />
+				</BrowserRouter>
+			</MockedProvider>,
+		);
+
+		await waitForElementToBeRemoved(() => queryByText('Loading...'));
+		expect(queryByText('Please verify your email.')).toBeInTheDocument();
+	});
+
+	it('renders without verify banner', async () => {
+		const { queryByText } = render(
 			<MockedProvider
 				mocks={[
 					{
@@ -30,10 +61,11 @@ describe('<Header />', () => {
 				<BrowserRouter>
 					<Header />
 				</BrowserRouter>
-				,
 			</MockedProvider>,
 		);
 
-		getByText('');
+		await waitForElementToBeRemoved(() => queryByText('Loading...'));
+		expect(queryByText('Please verify your email.')).not.toBeInTheDocument();
+
 	});
 });
