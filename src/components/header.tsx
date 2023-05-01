505703c1385bd3c779c52108fc8e23144c0b5762
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-05-01 19:12:04 -0400

    fix(components): add Loading component when the data of the logged user is been loaded

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,26 +1,30 @@
-import { FC } from 'react';
-
-import { useMe } from '../hooks/useMe';
-import nuberLogo from '../images/logo.svg';
+import React from "react";
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 
-export const Header: FC = () => {
-	const { data } = useMe();
+import { useMe } from '../hooks/useMe';
+import nuberLogo from '../images/logo.svg';
+
+export const Header: React.FC = () => {
+	const { data, loading } = useMe();
+
+	if(loading || !data) {
+		return <span>Loading...</span>
+	}
 
 	return (
 		<>
-			{!data?.me.verified && (
+			{!data.me.verified && (
 				<div className='bg-red-500 p-3 text-center text-base text-white'>
-					<span>Please verify your email.</span>
+					<span>Please verify your email.</span>					
 				</div>
 			)}
 			<header className='py-4'>
 				<div className='w-full px-5 xl:px-2 max-w-screen-xl mx-auto flex justify-between items-center'>
 					<Link to='/'>
 						<img src={nuberLogo} className='w-44' alt='Uber Eats' />
-					</Link>
+					</Link>				
 					<span className='text-xs'>
 						<Link to='/edit-profile'>
 							<FontAwesomeIcon icon={faUser} className='text-2xl' />
