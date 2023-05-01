36fa5efc1fba4040a77783933df94baa7cd00bfa
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-05-01 18:54:44 -0400

    test(): create unit test for App component

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,6 +1,6 @@
 import { useReactiveVar } from "@apollo/client";
-import { isLoggedInVar } from "../apollo";
 
+import { isLoggedInVar } from "../apollo";
 import { LoggedInRouter } from "../routers/logged-in-router";
 import { LoggedOutRouter } from "../routers/logged-out-router";
 
