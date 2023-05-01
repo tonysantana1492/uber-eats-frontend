d55e29c15c4908435c2aeaa0bf2a9a8d72ad8a2e
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-05-01 18:53:14 -0400

    test(): create unit test for Button component

--- src/components/__tests__/button.spec.tsx
+++ src/components/__tests__/button.spec.tsx
@@ -1,18 +1,17 @@
-import { render } from '@testing-library/react';
+import { render } from '../../utils/test-utils';
 import { Button } from '../button';
 
 describe('< Button />', () => {
 	it('should render OK with props', () => {
 		const { getByText } = render(<Button canClick={true} actionText='test' loading={false} />);
 
-		getByText('test');
-
+		expect(getByText('test')).toBeInTheDocument();
 	});
 
 	it('should should display loading OK with props', () => {
 		const { getByText, container } = render(<Button canClick={false} actionText='test' loading={true} />);
 
-		getByText('Loading...');
-        expect(container.firstChild).toHaveClass('pointer-events-none');
+		expect(getByText('Loading...')).toBeInTheDocument();
+		expect(container.firstChild).toHaveClass('pointer-events-none');
 	});
 });
