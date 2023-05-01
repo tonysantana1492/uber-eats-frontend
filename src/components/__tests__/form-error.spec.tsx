0fe903fc717bcecf5145f8c42cdc515964e49532
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-05-01 18:55:56 -0400

    test(): create unit test for FormError component

--- src/components/__tests__/form-error.spec.tsx
+++ src/components/__tests__/form-error.spec.tsx
@@ -1,10 +1,10 @@
 import { FormError } from '../form-error';
-import { render } from '@testing-library/react';
+import { render } from '../../utils/test-utils';
 
 describe('<FormError />', () => {
 	it('renders OK with props', () => {
 		const { getByText } = render(<FormError errorMessage='test' />);
 
-		getByText('test');
+		expect(getByText('test')).toBeInTheDocument();
 	});
 });
