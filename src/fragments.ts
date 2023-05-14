300a29313eae051572f08401bf353cbcb9d27d32
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-05-13 20:40:23 -0400

    feat(): create dish fragment

--- src/fragments.ts
+++ src/fragments.ts
@@ -1,7 +1,7 @@
 import { graphql } from './gql';
 
 // Este fragment es de tipo restaurant
-// Un fragment es una parte de un type
+// Un fragment es una parte de un type el cual puedo ver en el schema del server
 export const RESTAURANT_FRAGMENT = graphql(`
 	fragment RestaurantParts on Restaurant {
 		id
@@ -22,5 +22,48 @@ export const CATEGORY_FRAGMENT = graphql(`
 		coverImg
 		slug
 		restaurantCount
+	}
+`);
+
+export const DISH_FRAGMENT = graphql(`
+	fragment DishParts on Dish {
+		id
+		name
+		price
+		photo
+		description
+		options {
+			name
+			extra
+			choices {
+				name
+				extra
+			}
+		}
+	}
+`);
+
+export const ORDERS_FRAGMENT = graphql(`
+	fragment OrderParts on Order {
+		id
+		createdAt
+		total
+	}
+`);
+
+export const FULL_ORDER_FRAGMENT = graphql(`
+	fragment FullOrderParts on Order {
+		id
+		status
+		total
+		driver {
+			email
+		}
+		customer {
+			email
+		}
+		restaurant {
+			name
+		}
 	}
 `);
