490dbc892264e109c79ae432f82307aa9cd1389c
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-05-13 20:38:55 -0400

    build(): create adapter variables from env variables

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,4 @@
-export const LOCALSTORAGE_TOKEN = 'uber-token';
+export const APP_TOKEN_NAME = process.env.REACT_APP_TOKEN_NAME as string;
+export const APP_API = process.env.REACT_APP_API as string;
+export const APP_API_WS = process.env.REACT_APP_API_WS as string;
+export const APP_API_UPLOAD = process.env.REACT_APP_API_UPLOAD as string;
