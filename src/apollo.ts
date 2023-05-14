bdd9d8647c9c354d354c0c16fdba8935123b2e15
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-05-13 21:54:12 -0400

    build(): configurate wsLink to subscriptions

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,16 +1,26 @@
-import { ApolloClient, InMemoryCache, createHttpLink, makeVar } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink, makeVar, split } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-import { LOCALSTORAGE_TOKEN } from './constants';
+import { APP_API, APP_API_WS, APP_TOKEN_NAME } from './constants';
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 
-const token = localStorage.getItem(LOCALSTORAGE_TOKEN);
-
+const token = localStorage.getItem(APP_TOKEN_NAME);
 export const isLoggedInVar = makeVar(Boolean(token));
 export const authTokenVar = makeVar(token);
 
-const httpLink = createHttpLink({ uri: 'http://localhost:4000/graphql' });
+const httpLink = createHttpLink({ uri: APP_API });
+
+const wsLink = new WebSocketLink({
+	uri: APP_API_WS,
+	options: {
+	  reconnect: true,
+	  connectionParams: {
+		"x-jwt": authTokenVar() || "",
+	  },
+	},
+  });
 
 const authLink = setContext((_, { headers }) => {
-	
 	return {
 		headers: {
 			...headers,
@@ -19,25 +29,37 @@ const authLink = setContext((_, { headers }) => {
 	};
 });
 
+const splitLink = split(
+	({ query }) => {
+	  const definition = getMainDefinition(query);
+	  return (
+		definition.kind === "OperationDefinition" &&
+		definition.operation === "subscription"
+	  );
+	},
+	wsLink,
+	authLink.concat(httpLink)
+  );
+
 export const client = new ApolloClient({
-	link: authLink.concat(httpLink),
+	link: splitLink,
 	cache: new InMemoryCache({
 		typePolicies: {
 			// Define local fields (fields that are not in the schema of your server)
-			Query: {
-				fields: {
-					isLoggedIn: {
-						read() {
-							return isLoggedInVar();
-						},
-					},
-					token: {
-						read() {
-							return authTokenVar();
-						},
-					},
-				},
-			},
+			// Query: {
+			// 	fields: {
+			// 		isLoggedIn: {
+			// 			read() {
+			// 				return isLoggedInVar();
+			// 			},
+			// 		},
+			// 		token: {
+			// 			read() {
+			// 				return authTokenVar();
+			// 			},
+			// 		},
+			// 	},
+			// },
 		},
 	}),
 });
