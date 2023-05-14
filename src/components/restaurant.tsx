684eac6978295138ca9fb96d2c244295755ee5ae
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-05-13 21:02:35 -0400

    fix(components): change url background image format

--- src/components/restaurant.tsx
+++ src/components/restaurant.tsx
@@ -11,7 +11,7 @@ interface IRestaurantProps {
 export const Restaurant: FC<IRestaurantProps> = ({ id, coverImg, name, categoryName }) => (
 	<Link to={`/restaurant/${id}`}>
 		<div className='flex flex-col'>
-			<div style={{ backgroundImage: `url(/restaurants/${coverImg})` }} className='bg-cover bg-center mb-3 py-28'></div>
+			<div style={{ backgroundImage: `url(${coverImg})` }} className='bg-cover bg-center mb-3 py-28'></div>
 			<h3 className='text-xl'>{name}</h3>
 			<span className='border-t mt-2 py-2 text-xs opacity-50 border-gray-400'>{categoryName}</span>
 		</div>
