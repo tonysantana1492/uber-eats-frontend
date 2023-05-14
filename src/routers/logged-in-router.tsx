9855ae19bbb53f6d4323bb757746701c80e66501
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-05-13 21:30:19 -0400

    feat(routers): add order router

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -11,12 +11,16 @@ import { Category } from '../pages/client/category';
 import { Restaurant } from '../pages/client/restaurant';
 import { MyRestaurants } from '../pages/owner/my-restaurants';
 import { AddRestaurant } from '../pages/owner/add-restaurants';
+import { MyRestaurant } from '../pages/owner/my-restaurant';
+import { AddDish } from '../pages/owner/add-dish';
+import { Order } from '../pages/order';
 
 const routes = [
 	// Common
 	{ path: '/confirm', element: <ConfirmEmail />, roles: [] },
 	{ path: '/edit-profile', element: <EditProfile />, roles: [] },
 	{ path: '*', element: <NotFound />, roles: [] },
+	{ path: '/order/:id', element: <Order />, roles: [] },
 	// Client
 	{
 		path: '/',
@@ -45,8 +49,18 @@ const routes = [
 		roles: [UserRole.Owner],
 	},
 	{
+		path: 'restaurant/:id',
+		element: <MyRestaurant />,
+		roles: [UserRole.Owner],
+	},
+	{
 		path: '/add-restaurant',
 		element: <AddRestaurant />,
+		roles: [UserRole.Owner]
+	},
+	{
+		path: '/restaurant/:restaurantId/add-dish',
+		element: <AddDish />,
 		roles: [UserRole.Owner]
 	}
 ];
